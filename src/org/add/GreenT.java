d39a3a633d821b049427185861a351c3bcea4d7b
username <madhanag08@gmail.com>
2024-07-02 11:13:02 +0530

    asdfghj

---
+++ src/org/add/GreenT.java
@@ -0,0 +1,34 @@
+package org.add;
+
+public class GreenT {
+	private void greensVelacherry() {
+		System.out.println("Besant");
+		// TODO Auto-generated method stub
+
+	}
+	private void greensAdyar() {
+		System.out.println("Info");
+		// TODO Auto-generated method stub
+
+	}
+	private void greensTambaram() {
+		System.out.println("view");
+		// TODO Auto-generated method stub
+
+	}
+	private void greensAnnanagar() {
+		System.out.println("cts");
+		// TODO Auto-generated method stub
+
+	}
+	//main method creation
+	public static void main(String[]args) {
+		GreenT t = new GreenT();
+		t.greensVelacherry();
+		t.greensAdyar();
+		t.greensTambaram();
+		t.greensAnnanagar();
+				
+	}
+
+}
---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,30 @@
+package org.add;
+
+public class GreensTech {
+	public void greensVelacherry() {
+		System.out.println("testing");
+		
+	}
+	public void greensAdyar() {
+		System.out.println("oracle");
+		
+	}
+	public void greensTambaram() {
+		System.out.println("azure");
+	}
+	public void greensAnnanagar() {
+		System.out.println("java");
+	}
+	
+	//main method creation
+	public static void main (String[]args) {
+		GreensTech g= new GreensTech();
+		g.greensVelacherry();
+		g.greensAdyar();
+		g.greensTambaram();
+		g.greensAnnanagar();
+				
+				
+				
+	}
+}
